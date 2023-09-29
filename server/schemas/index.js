b39712bdfc2668c8b2c74f41ccae6a08d17d08a9
Khalid G <kgibson4795@gmail.com>
2023-09-29 10:01:27 -0400

    backend routing and setup

---
+++ server/schemas/index.js
@@ -0,0 +1,10 @@
+// const { Matchup, Tech } = require('../models');
+
+
+
+
+
+const typeDefs = require('./typeDefs')
+const resolvers = require('./resolvers')
+
+module.exports = { typeDefs, resolvers };
